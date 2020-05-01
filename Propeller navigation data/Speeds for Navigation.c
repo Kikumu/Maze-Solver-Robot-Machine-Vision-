dfd8264456c862e7f1eb16f6200e2f2351720de6
Ten5ei <scowty.sk@outlook.com>
2020-05-01 23:50:28 +0100

    movement calibration

--- Propeller navigation data/Speeds for Navigation.c
+++ Propeller navigation data/Speeds for Navigation.c
@@ -1,58 +1,50 @@
-/* 
-  Speeds for Navigation.c
-  
-  Navigate by making your ActivityBot go certain speeds for certain amounts
-  of time.
-
-  http://learn.parallax.com/activitybot/set-certain-speeds
-*/
-
-
 #include "simpletools.h"                      // simpletools library
 #include "abdrive.h"                          // abdrive library
 #include "adcDCpropab.h" 
 
-
-int distLeft[4], distRight[4];
 int main()                   
 {
-//adc_init(21, 20, 19, 18); 
+adc_init(21, 20, 19, 18); 
 int var = 5;
-//float v1,v2,v3,v0,turn_around
-int v1,v2,v3,v0,turn_around,status;
-drive_getTicks(&distLeft[0], &distRight[0]);
-print("distLeft[0] = %d, distRight[0] = %d\n", distLeft[0], distRight[0]);
-int count = 0;
-v1 = input(1);
-v2 = input(0);
-v3 = input(2);
+float v0,v1,v2,v3,turn_around,status;
+//26,13,5
 //--------------------------------------loop-----------------------------------------------//
  while (var < 7){
 turn_around = input(10);
-v1 = input(1); //16
-v2 = input(0); //20
-v3 = input(2); //26
-
-print("A/D3 = %f V%c\n", v3, CLREOL);      // Display volts
-//forward(16)
-if(v1 == 0){
-  status = 0;
-  drive_speed(0, 0);
-  }
-else if(v1 == 1){
-  status = 1;
+//v2 = input(1);
+//v1 = adc_volts(2);
+//v0=  adc_volts(1);
+v0 = input(0);//26
+v1 = input(1);//13
+v2 = input(2);//5(6 for the model b pi3)
+//forward
+if(v0 > 0){
   drive_speed(25, 25);
- }
-//turn_around = 21
-else if(turn_around==1){
-    drive_speed(45, 0);                       
-    drive_speed(0, -45);
+  }
+else if(v0 < 1){
+  drive_speed(0, 0);
 }
-//left(20)
-else if(v2==1){drive_speed(25, 0);}  
- //right(26)
-else if(v3==1){drive_speed(0, 25);}  
-//print("A/D3 = %f V%c\n", v1, CLREOL);     // Display volts
-pause(1000);
+ 
+//adjust left
+if(v1 > 0){
+    drive_speed(25, 0);
+}
+//else if(v1 < 1){
+//  drive_speed(0, 0);
+//}  
+
+
+//adjust right
+if(v2 > 0)
+{
+  drive_speed(0, 25);
+} 
+//else if(v2 < 1)
+//{
+//   drive_speed(0, 0);
+//}  
+ 
+//print("A/D3 = %f V%c\n", v2, CLREOL);     // Display volts
+//pause(1000);
  }    
 }
