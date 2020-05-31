5b7780dd7e39f771dc34bf497884273d1cad6d9f
Ten5ei <scowty.sk@outlook.com>
2020-05-31 15:45:58 +0100

    --

--- Propeller navigation data/Speeds for Navigation.c
+++ Propeller navigation data/Speeds for Navigation.c
@@ -8,7 +8,7 @@ int var = 5;
 float v0,v1,v2,v3,v4,v7;
 //26,13,5
 //--------------------------------------loop-----------------------------------------------//
-while (var < 7){
+ while (var < 7){
 v0 = input(0);//26
 v1 = input(1);//13
 v2 = input(2);//5(6 for the model b pi3)
@@ -20,31 +20,31 @@ v7 = input(7);//23
 //forward
 if(v0 > 0){
   high(5);
-  drive_speed(30, 30);
+  drive_ramp(30, 30);
   }
 else if(v0 < 1){
   low(5);
-  drive_speed(0, 0);
+  drive_ramp(0, 0);
 }
 //adjust left
 v1 = input(1);
 if(v1 > 0){
     high(5);
-    drive_speed(15, 0);
+    drive_ramp(15, 0);
 }
 //adjust right
 v2 = input(2);
 if(v2 > 0)
 {
   high(5);
-  drive_speed(0, 15);
+  drive_ramp(0, 15);
 } 
 //turn right 45
 v3 = input(3);
 if (v3 > 0)
 {
   high(5);//12
-  drive_speed(25,25);
+  drive_ramp(25,25);
   pause(400);
   drive_speed(0,0);
   drive_speed(0,50); //52
@@ -56,7 +56,7 @@ v7 = input(7);
 //turn left 45
 if (v7 > 0){
   high(5);
-  drive_speed(25,25);
+  drive_ramp(25,25);
   pause(400);
   drive_speed(0,0);
   drive_speed(50,0); //52
